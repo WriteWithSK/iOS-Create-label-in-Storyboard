951dd9478876ec891035164527b4cd3eb647296d
Saravanakumar Murugesan <smurugesan@ntst.com>
2023-03-13 11:23:37 +0530

    Create label in storyboard

---
+++ createlabel/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  createlabel
+//
+//  Created by Write with SK on 23/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = "new label new label new label hello world"
+    }
+
+
+}
+
